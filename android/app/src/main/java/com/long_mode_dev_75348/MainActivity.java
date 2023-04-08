36bc3f7e0d5e7adb0579711c23e7ed20e4542cea
Crowdbotics <engineering@crowdbotics.com>
2023-04-08 01:11:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/long_mode_dev_75348/MainActivity.java
@@ -0,0 +1,15 @@
+package com.long_mode_dev_75348;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "long_mode_dev_75348";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-mode-dev-75348",
+  url: "https://long-mode-dev-75348.botics.co",
+  api: "https://long-mode-dev-75348.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
